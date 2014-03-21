af05a07f51f27795b1ef1c5d6482c7ac3bfcf29d
grasGendarme <ldninten@yahoo.fr>
2014-03-21 15:48:08 +0100

    Convert to ES6 let syntax where possible

--- routes/new.js
+++ routes/new.js
@@ -1,30 +1,33 @@
+'use strict';
+
 var messages = require('../lib/messages.js');
 var _ = require('underscore');
 
 var MAXMESSAGELENGTH = 160;
 
 module.exports = function *newContentHandler(next) {
-	if ('/new' == this.path) {
-		if (this.query && this.query.content) {
+  if ('/new' !== this.path) {
+    yield next;
+  } else {
+    if (this.query && this.query.content) {
         if(this.query.content.length < MAXMESSAGELENGTH) {
         this.query.content = _.escape(this.query.content);
+
         // regex magic to parse links posted
-        var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+        let exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
         this.query.content = this.query.content.replace(exp,"<a href='$1' rel=\"nofollow\">$1</a>"); 
-		    messages.push(this.query.content, new Date());
-		    this.status = 303;
-        this.set('Location', '/');
+        messages.push(this.query.content, new Date());
+        this.status = 200;
         return;
       } else {
         this.body = 'Message too large';
         this.status = 400;
         return;
       }
-		} else {
-		  this.body = 'Invalid request';
-		  this.status = 400;
-		}
+    } else {
+      this.body = 'Invalid request';
+      this.status = 400;
+    }
     return;
-	}
-	yield next;
+  }
 };
